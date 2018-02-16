b691b4867ee19977dcaad3347a18bc048b2bf36f
Derrick Hathaway <derr@me.com>
2018-02-16 11:37:28 -0700

    Initial Commit

---
+++ EpicGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EpicGame
+//
+//  Created by Derrick Hathaway on 2/16/18.
+//  Copyright © 2018 Derrick Hathaway. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
